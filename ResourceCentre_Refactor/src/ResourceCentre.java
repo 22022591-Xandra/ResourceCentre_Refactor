963804ed7386d2ce42ba5ca5b2e2a62bbf48df00
Vincent <Vincent@10.175.4.211>
2023-07-21 13:55:39 +0800

    chromebookList change

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -154,7 +154,7 @@ public class ResourceCentre {
 
 		for (int i = 0; i < camcorderList.size(); i++) {
 
-			output += String.format("%=84s \n", camcorderList.get(i).toString());
+			output += String.format("%=84s\n", camcorderList.get(i).toString());
 		}
 		return output;
 	}
@@ -172,10 +172,7 @@ public class ResourceCentre {
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
-					chromebookList.get(i).getDescription(), 
-					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
-					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
+			output += String.format("%=84s\n", chromebookList.get(i).toString());
 		}
 		return output;
 	}
